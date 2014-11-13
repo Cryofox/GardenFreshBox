2c6f02b38667e4b3d3a118813f33fa0ebf4cc5f7
Cryofox <hstances@uoguelph.ca>
2014-11-12 20:01:05 -0500

    Mike's pyramid setup + foundation

---
+++ gfb/controller.py
@@ -0,0 +1,19 @@
+from pyramid.httpexceptions import HTTPFound
+import hashlib
+
+def process_login_request(request):
+	if not 'user' in request.params or not 'pass' in request.params or request.params['user'] == '' or request.params['pass'] == '':
+		return HTTPFound(location='login?error=1')
+	username = request.params['user']
+	password = hashlib.sha224(request.params['pass'].encode('utf-8')).hexdigest()
+	with open('auth/users.auth') as f:
+		for line in f.readlines():
+			auth = line.split(';')
+			if username.strip() == auth[0].strip() and password.strip() == auth[1].strip():
+				request.session['username'] = username
+				return HTTPFound(location="/")
+	return HTTPFound(location="login?error=2")
+
+def process_logout_request(request):
+	del request.session['username']
+	return HTTPFound("/")
---
+++ gfb/views.py
@@ -0,0 +1,60 @@
+from pyramid.renderers import get_renderer
+from pyramid.view import view_config
+from controller import (
+	process_login_request,
+	process_logout_request
+)
+
+def site_layout():
+	renderer = get_renderer("templates/core_layout.pt")
+	return renderer.implementation().macros['layout']
+
+'''
+get the specific header for the person logged in
+'''
+def user_header(request):
+	if not 'username' in request.session:
+		with open('templates/user_header.pt') as f:
+			return f.read()
+	else:
+		with open('templates/admin_header.pt') as f:
+			return f.read()
+
+@view_config(renderer="templates/index.pt")
+def index_view(request):
+	#if the user is logged in redirect to the appropriate home page
+	return {"layout": site_layout(), "user_header": user_header(request), "location": "Home"}
+
+@view_config(renderer="templates/index.pt")
+def foundation_view(request):
+	#if the user is logged in redirect to the appropriate home page
+	return {"layout": site_layout(), "user_header": user_header(request), "location": "Foundation"}
+
+
+
+@view_config(renderer="templates/login.pt", name="login")
+def login_view(request):
+	if 'username' in request.params: #there's gotta be a better way to do page permissions
+		return HTTPFound("/")
+	if 'login.submit' in request.params and request.params['login.submit'] == 'login':
+		return process_login_request(request)
+	ret = {"layout": site_layout(), "user_header": user_header(request), "location": "Log In"}
+	if 'error' in request.params: #if there was a login error give a meaningful message
+		if request.params['error'] == '1':
+			ret['error1'] = True
+		elif request.params['error'] == '2':
+			ret['error2'] = True
+	return ret
+
+@view_config(name="logout")
+def logout_view(request):
+	return process_logout_request(request)
+
+
+
+	
+@view_config(renderer="templates/signup.pt", name="signup")
+def signup_view(request):
+	if 'username' in request.params: #there's gotta be a better way to do page permissions
+		return HTTPFound("/")
+	return {"layout": site_layout(), "user_header": user_header(request), "location": "Sign Up"}
